aee13920671295a864b4eae75c98ab6070dcccaa
kabolas <suri.mur.bry@gmail.com>
2017-11-02 15:50:49 +0100

    changement et reparation de beaucoup de choses commencement de l'ajout des classes à l'UI

--- Project2/Project2/MyForm.cpp
+++ Project2/Project2/MyForm.cpp
@@ -8,14 +8,6 @@ using namespace System::Windows::Forms;
 [STAThreadAttribute]
 void Main(array<String^>^ args) 
 {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-
-	std::vector<std::string> buf;
-	
-	//ProblemVLS problem;
-	//problem.printStation(1);
-	//problem.printAllStations();
 	
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
